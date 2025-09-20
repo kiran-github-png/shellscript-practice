268cd4f69fd734f475400a1d9b1213aba3a59217
kirankumar kosuri <kosurikirankumar7@gmail.com>
2025-09-20 19:18:49 +0530

    03-variables.sh

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=PAVAN
+PERSON2=KUMAR
+
+echo "$PERSON1:: hi $PERSON2 how are you"
+echo "$PERSON2:: im fine $PERSON1 what about you"
+echo "$PERSON1:: what are you learning currently $PERSON2"
+echo "$PERSON2 im learning shell script $PERSON1"
