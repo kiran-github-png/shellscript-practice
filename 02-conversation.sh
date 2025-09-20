fbb15c2126d9bb7628c58c2a95db4740041f3a25
kirankumar kosuri <kosurikirankumar7@gmail.com>
2025-09-20 19:14:05 +0530

    02-conversation.sh

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "kiran:: hi praveen how are you"
+echo "praveen:: im fine kiran what about you"
+echo "kiran:: what are you learning currently praveen"
+echo "praveen im learning shell script kiran"
