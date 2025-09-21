bfe88b30dade7d2311216f3875585b3681b98d09
kirankumar kosuri <kosurikirankumar7@gmail.com>
2025-09-21 15:31:36 +0530

    07-env-variables.sh

--- 07-env-variables.sh
+++ 07-env-variables.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-DATE=$(date)
-echo "Timestamp executed: $DATE"
+#DATE=$(date)
+ 
+ START_TIME= $(date +%s)
+ sleep 10
+ END_TIME= $(($END_TIME-START_TIME))
+ echo "script executed in: $TOTAL_TIME seconds"
