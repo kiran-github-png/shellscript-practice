7f08652953c76696925de7c5da83252ed9365349
kirankumar kosuri <kosurikirankumar7@gmail.com>
2025-09-21 11:12:05 +0530

    04-variables.sh

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ 
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: hi welcome to alaska $PERSON2"
+echo "$PERSON2:: thank you $PERSON1"
+echo "$PERSON1:: thank you $PERSON2"
+echo "$PERSON1:: welcome $PERSON2"
+echo "$PERSON2:: ok $PERSON1"
