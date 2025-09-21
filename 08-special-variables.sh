b4ebd4bca6752c026ee2e5fdb0ebe86ac4420263
kirankumar kosuri <kosurikirankumar7@gmail.com>
2025-09-21 16:19:08 +0530

    08-special-variables.sh

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -2,3 +2,8 @@
 
 echo "all variables passed to the script: $@"
 echo "all variables passed to the script: $*"
+echo "script name: $0"
+echo "current directory: $PWD"
+echo "wgo is running this: $USER"
+eecho "home directory of user: $HOME"
+ECHO "PID of this script: $$"
