1fdc484cb94158f461c7684f0a44b38a8eb8b03b
kirankumar kosuri <kosurikirankumar7@gmail.com>
2025-09-22 13:01:11 +0530

    10-data-types.sh

--- 10-data-types.sh
+++ 10-data-types.sh
@@ -3,9 +3,9 @@
 NUMBER=$!
 
 if [ $NUMBER -lt 10 ]; then
-echo "Given Number is $NUMBER less than 10"
+   echo "Given Number is $NUMBER less than 10"
 elif [ $NUMBER -eq 10 ]; then
-echo "Given Number is $NUMBER equal to 10"
+     echo "Given Number is $NUMBER equal to 10"
 else 
 echo "Given Number is $NUMBER greater than 10"
 fi
