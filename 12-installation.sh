1079a8e8702473d4402f8ed795c145938a9fb834
kirankumar kosuri <kosurikirankumar7@gmail.com>
2025-09-23 13:25:16 +0530

    12-installation.sh

--- 12-installation.sh
+++ 12-installation.sh
@@ -2,11 +2,13 @@
 USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
   echo "ERROR:: PLEASE RUN THIS SCRIPT WITH ROOT PRIVELAGE"
+  exit 1
 fi
 
 dnf install mysqld -y
 if [ $? -ne 0 ]; then
   echo "ERROR:: Installing mysql is failure"
+  exit 1
 else
   echo "Installing mysql is SUCCESS"
 fi
