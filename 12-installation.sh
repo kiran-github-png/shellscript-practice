eb3a58b50a180d3e1f62f82330b4da263bdd563d
kirankumar kosuri <kosurikirankumar7@gmail.com>
2025-09-23 14:54:33 +0530

    12-installation.sh

--- 12-installation.sh
+++ 12-installation.sh
@@ -1,23 +1,46 @@
 #!/bin/bash
 USERID=$(id -u)
-if [ $USERID -ne 0 ]; then
-  echo "ERROR:: PLEASE RUN THIS SCRIPT WITH ROOT PRIVILAGE"
-  exit 1
-fi 
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e{34m"
+
+if [ $USERID -ne 0 ]; then  
+  echo"ERROR:: PLEASE RUN THIS SCRIPT WITH ROOT PRIVELAGE"
+  ecit 1
+fi
 
 VALIDATE(){
-  if [ $1 -ne 0 ]; then
-    echo "ERROR:: Installing $2 is failure"
-    exit 1
+if [ $1 -ne 0 ]; then
+  echo -e "Installing $2 ... $R FAILURE $N"
+  exit 1
   else
-    echo "Installing $2 is SUCCESS"
+    echo -e "Installing $2 ... $G   SUCCESS"
   fi
-}  
-   dnf install mysql -y
-   VALIDATE $? "mysql"
+}
+dnf list install mysql
+ if [ $? -ne 0 ]; then
+dnf install mysql -y
+VALIDATE $? "mysql"
+else  
+  echo "MYSQL already exit ... $Y skipping $N"
+  fi
 
-   dnf install nginx -y
-   VALIDATE $? "nginx"
+  }
+dnf list install nginx
+ if [ $? -ne 0 ]; then
+dnf install nginx -y
+VALIDATE $? "nginx"
+else  
+  echo "MYSQL already exit ... $Y skipping $N"
+  fi
 
-   dnf install python3 -y
-   VALIDATE $? "python3"
+  }
+dnf list install python3
+ if [ $? -ne 0 ]; then
+dnf install python3 -y
+VALIDATE $? "python3"
+else  
+  echo "MYSQL already exit ... $Y skipping $N"
+  fi
