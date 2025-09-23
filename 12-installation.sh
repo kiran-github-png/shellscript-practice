9151ab14eb3907abe52a90b28f129d3394acc554
kirankumar kosuri <kosurikirankumar7@gmail.com>
2025-09-23 15:04:12 +0530

    12-installation.sh

--- 12-installation.sh
+++ 12-installation.sh
@@ -1,44 +1,46 @@
 #!/bin/bash
-USERID=$(id -u)
 
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-N="\e{34m"
+N="\e[0m"
 
-if [ $USERID -ne 0 ]; then  
-  echo"ERROR:: PLEASE RUN THIS SCRIPT WITH ROOT PRIVELAGE"
-  ecit 1
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
 
-VALIDATE(){
-if [ $1 -ne 0 ]; then
-  echo -e "Installing $2 ... $R FAILURE $N"
-  exit 1
-  else
-    echo -e "Installing $2 ... $G   SUCCESS"
-  fi
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "Installing $2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "Installing $2 ... $G SUCCESS $N"
+    fi
 }
-dnf list install mysql
- if [ $? -ne 0 ]; then
-dnf install mysql -y
-VALIDATE $? "mysql"
-else  
-  echo -e "MYSQL already exit ... $Y skipping $N"
-  fi
 
-dnf list install nginx
- if [ $? -ne 0 ]; then
-dnf install nginx -y
-VALIDATE $? "nginx"
-else  
-  echo -e "MYSQL already exit ... $Y skipping $N"
-  fi
+dnf list installed mysql
+# Install if it is not found
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+    VALIDATE $? "MySQL"
+else
+    echo -e "MySQL already exist ... $Y SKIPPING $N"
+fi
 
-dnf list install python3
- if [ $? -ne 0 ]; then
-dnf install python3 -y
-VALIDATE $? "python3"
-else  
-  echo -e "MYSQL already exit ... $Y skipping $N"
-  fi
+dnf list installed nginx
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+    VALIDATE $? "Nginx"
+else
+    echo -e "Nginx already exist ... $Y SKIPPING $N"
+fi
+
+dnf list installed python3
+if [ $? -ne 0 ]; then
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo -e "Python3 already exist ... $Y SKIPPING $N"
+fi
