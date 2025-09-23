fdbd640bc3ed8dab47fbae8494d8b2c9b5ac429e
kirankumar kosuri <kosurikirankumar7@gmail.com>
2025-09-23 13:41:20 +0530

    12-installation.sh

--- 12-installation.sh
+++ 12-installation.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 USERID=$(id -u)
-if [ $USERID -ne 0 ]; then
-  echo "ERROR:: PLEASE RUN THIS SCRIPT WITH ROOT PRIVELAGE"
+if [USERID -ne 0 ]; then
+  echo "ERROR:: PLEASE RUN THIS SCRIPT WITH ROOT PRIVILAGE"
   exit 1
-fi
+fi 
 
-dnf install mysqld -y
-if [ $? -ne 0 ]; then
-  echo "ERROR:: Installing mysql is failure"
-  exit 1
-else
-  echo "Installing mysql is SUCCESS"
-fi
+VALIDATE(){
+  if [ $1 -ne 0 ]; then
+    echo "ERROR:: Installing $2 is failure"
+    exit 1
+  else
+    echo "Installing $2 is SUCCESS"
+  fi
+}  
+   dnf install mysql -y
+   VALIDATE $? "mysql"
+
+   dnf install nginx -y
+   VALIDATE $? "nginx"
+
+   dnf install python3 -y
+   VALIDATE $? "python3"
