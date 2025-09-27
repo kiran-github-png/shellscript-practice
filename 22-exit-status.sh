cdaca87ab8ef0b00f8304701b7b5181e03a42a9e
kirankumar kosuri <kosurikirankumar7@gmail.com>
2025-09-27 16:59:46 +0530

    22-exit-status.sh

--- 22-exit-status.sh
+++ 22-exit-status.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-lss -ltr 
+ls -ltr 
  if [ $? -ne 0 ]; then
     echo "Previous Command is Failure"
     exit 1
  fi
 
- 
+ if [ $? -ne 0 ]; then
+    echo "Previous Command is Failure"
+    exit 1
+fi
+
+    echo "Program is success"
